6ba1ce1d25022e64997f834a10ec23bea8562cf1
Gian Comunello <gianmcomu@gmail.com>
2023-12-19 12:49:05 -0300

    agrego validaciones al formulario

--- src/componentes/CheckoutFormulario/CheckoutForm.jsx
+++ src/componentes/CheckoutFormulario/CheckoutForm.jsx
@@ -4,13 +4,44 @@ export default function CheckoutForm({onConfirm}) {
     const [nombre, setNombre]= useState("")
     const [telefono, setTelefono]= useState("")
     const [email, setEmail]=useState("")
+    const [mostrarError, setMostrarError]= useState(false);
+    function validarFormulario(){
+        const soloLetrasRegex = /^[A-Za-z]+$/;
+        const soloNumerosRegex = /^\d+$/;
+            if (nombre === "" || !soloLetrasRegex.test(nombre)) {
+                console.log("Error en el nombre");
+              setMostrarError(true);
+      return false;
+            }
+            
+            if (telefono === "" || !soloNumerosRegex.test(telefono)) {
+                console.log("Error en el tel");
+              setMostrarError(true);
+      return false;
+            }
+        
+            if (email === "") {
+                console.log("Error en el mail");
+              setMostrarError(true);
+              return false;
+            } 
+            return true;
+    }
 
     const handleConfirm=(event)=>{
         event.preventDefault()
-        const userData={
-            nombre,telefono,email
+        const esFormularioValido = validarFormulario()
+        setNombre("");
+        setTelefono("");
+        setEmail("");
+        if(esFormularioValido){
+            console.log("Formulario completado con éxito!!");
+            const userData={
+                nombre,telefono,email
+            }
+            onConfirm(userData)
         }
-        onConfirm(userData)
+       
     }
   return ( 
     <div className={style.contenedorForm}>
@@ -19,6 +50,7 @@ export default function CheckoutForm({onConfirm}) {
                 Nombre
                 <input type="text"
                 className={style.input}
+                placeholder="Debe colocar su nombre"
                 value={nombre}
                 onChange={({target})=> setNombre(target.value)} />
             </label>
@@ -26,6 +58,7 @@ export default function CheckoutForm({onConfirm}) {
                 Telefono
                 <input type="text"
                 className={style.input}
+                placeholder="Debe colocar su numero de telefono"
                 value={telefono}
                 onChange={({target})=> setTelefono(target.value)} />
             </label>
@@ -33,9 +66,13 @@ export default function CheckoutForm({onConfirm}) {
                 Email
                 <input type="email"
                 className={style.input}
+                placeholder="Debe colocar su email"
                 value={email}
                 onChange={({target})=> setEmail(target.value)} />
             </label>
+            {mostrarError && (
+          <p className={style.error}>Por favor, completa todos los campos correctamente.</p>
+        )}
             <div className={style.crearOrden}>
                 <button type='submit' className={style.botonCrearOrden}>Crear orden</button>
             </div>
